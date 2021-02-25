86888e2d7a0105157f68a2056aae76b42ae1217a
C Helder <christian.helder@itvitaelearning.nl>
2021-02-25 16:15:40 +0100

    RepairOrder Parts Verwerkt in CreateView en begin Details

--- Razor_Identity/Pages/RepairOrders/Details.cshtml.cs
+++ Razor_Identity/Pages/RepairOrders/Details.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Razor_Identity.Data;
 using Razor_Identity.Models;
@@ -34,6 +35,17 @@ namespace Razor_Identity.Pages.RepairOrders
             {
                 return NotFound();
             }
+
+            //Maak dit logischer en kleiner
+            var RepairOrderPartID1 = RepairOrder.PartID1;
+            var PartID1 = (from p in _context.Parts where p.Id == RepairOrderPartID1 select p.Name).ToList();
+            
+            foreach (string str in PartID1)
+            {
+                ViewData["PartID1"] = str;
+            }
+            //ViewData["PartID1"] = PartID1;
+
             return Page();
         }
     }
